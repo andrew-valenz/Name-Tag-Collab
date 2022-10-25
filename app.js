dc7fba37271a8e91a55580e8242ae6d6ed5df89f
Aaron Ibarra <aaronibarra132@gmail.com>
2022-10-25 16:42:32 -0700

    added functionality for name display color change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const nameDisplay = document.getElementById('name');
 console.log('nameDisplay', nameDisplay);
 
 nameDisplay.textContent = 'Aaron';
-nameDisplay.classList.add('Orange');
+nameDisplay.classList.add('orange');
 
 const updateBtn = document.getElementById('update-btn');
 updateBtn.addEventListener('click', () => {
@@ -10,4 +10,15 @@ updateBtn.addEventListener('click', () => {
     const nameInput = document.getElementById('name-input');
     nameDisplay.textContent = nameInput.value;
     nameInput.value = '';
+});
+
+const colorDropdown = document.getElementById('color-dropdown');
+colorDropdown.addEventListener('change', () => {
+    console.log('change is happening bro');
+    console.log(colorDropdown.value);
+    const nameTagSection = document.getElementById('name-tag-section');
+    nameDisplay.classList.remove('orange');
+    nameDisplay.classList.remove('purple');
+    nameDisplay.classList.remove('brown');
+    nameDisplay.classList.add(colorDropdown.value);
 });
