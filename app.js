02f867673e5495bf0798c8c7330c7571640adf47
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2022-10-25 16:14:46 -0700

    Event Listener button

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 const nameDisplay = document.getElementById('name');
 console.log('nameDisplay', nameDisplay);
+
+nameDisplay.textContent = 'Aaron';
+nameDisplay.classList.add('Orange');
+
+const updateBtn = document.getElementById('update-btn');
+updateBtn.addEventListener('click', () => {
+    console.log('i clicked this button');
+    const nameInput = document.getElementById('name-input');
+    nameDisplay.textContent = nameInput.value;
+    nameInput.value = '';
+});
